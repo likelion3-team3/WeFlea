3bd4cae1b8a01f40f9d0007b0fb29e6b17d3b49e
Jihn0118 <lantern50@tukorea.ac.kr>
2023-05-25 15:43:07 +0900

    Initial project setup

---
+++ src/main/java/com/ll/weflea/WefleaApplication.java
@@ -0,0 +1,13 @@
+package com.ll.weflea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WefleaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WefleaApplication.class, args);
+	}
+
+}
