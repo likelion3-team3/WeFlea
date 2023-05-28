135473f10817724e4bc9040400e99854acf7cb34
Hypersand <on8214@gmail.com>
2023-05-28 20:49:20 +0900

    feat: 기본 로그인 화면 임시 제거

--- src/main/java/com/ll/weflea/WefleaApplication.java
+++ src/main/java/com/ll/weflea/WefleaApplication.java
@@ -2,9 +2,10 @@ package com.ll.weflea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // 기본 로그인 화면 임시 제거
 @EnableJpaAuditing
 public class WefleaApplication {
 
