e35ae3984128b46ec8a12024676309ec506564a9
Hypersand <on8214@gmail.com>
2023-06-02 10:32:40 +0900

    fix: 인터셉터 인증 처리 로직 수정

--- src/main/java/com/ll/weflea/base/interceptor/NicknameCheckInterceptor.java
+++ src/main/java/com/ll/weflea/base/interceptor/NicknameCheckInterceptor.java
@@ -5,6 +5,7 @@ import com.ll.weflea.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class NicknameCheckInterceptor implements HandlerInterceptor {
 
     private final MemberService memberService;
@@ -20,7 +22,14 @@ public class NicknameCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null || authentication.getPrincipal() instanceof User) {
+
+        if (authentication == null) {
+            response.sendRedirect("/user/member/login");
+            return false;
+        }
+
+
+        if (authentication.getPrincipal() instanceof User) {
             User user = (User)authentication.getPrincipal();
             String username = user.getUsername();
             Member member = memberService.findByUsername(username).orElse(null);
