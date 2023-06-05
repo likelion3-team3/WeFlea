428af218e4edfda194c7a016a697c3b7df06aa9b
Hypersand <on8214@gmail.com>
2023-06-05 13:38:01 +0900

    fix: 배포 서버에서 layout 못 불러오는 문제 해결

--- src/main/java/com/ll/weflea/base/webSocket/WebSocketConfig.java
+++ src/main/java/com/ll/weflea/base/webSocket/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.ll.weflea.base.webSocket;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.*;
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${custom.site.baseUrl}")
+    private String baseUrl;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("http://localhost:8080").withSockJS();
+                .setAllowedOrigins(baseUrl).withSockJS();
     }
 
     /*어플리케이션 내부에서 사용할 path를 지정할 수 있음*/
--- src/test/java/com/ll/weflea/WefleaApplicationTests.java
+++ src/test/java/com/ll/weflea/WefleaApplicationTests.java
@@ -3,7 +3,7 @@ package com.ll.weflea;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WefleaApplicationTests {
 
 	@Test
