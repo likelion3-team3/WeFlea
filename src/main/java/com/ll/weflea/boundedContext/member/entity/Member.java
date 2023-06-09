5bd97d25903484586f3fdae6107316a956ee55ef
Hypersand <on8214@gmail.com>
2023-06-09 22:07:16 +0900

    feat: 토스페이먼츠 결제 임시 구현

--- src/main/java/com/ll/weflea/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/weflea/boundedContext/member/entity/Member.java
@@ -33,6 +33,8 @@ public class Member extends BaseEntity {
     @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private ProfileImage profileImage;
 
+    private Long point;
+
     public List<? extends GrantedAuthority> getGrantedAuthorities() {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
 
