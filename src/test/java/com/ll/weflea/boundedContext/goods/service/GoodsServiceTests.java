b74711e78f621a842aa723708fe1902f02ae470c
깃wooqqq <sge0225@naver.com>
2023-06-12 03:30:38 +0900

    test : 상품 목록 페이지 확인을 위해 테스트케이스 생성

--- src/test/java/com/ll/weflea/boundedContext/goods/service/GoodsServiceTests.java
+++ src/test/java/com/ll/weflea/boundedContext/goods/service/GoodsServiceTests.java
@@ -1,4 +1,38 @@
 package com.ll.weflea.boundedContext.goods.service;
 
+import com.ll.weflea.base.rsData.RsData;
+import com.ll.weflea.boundedContext.goods.controller.GoodsController;
+import com.ll.weflea.boundedContext.goods.entity.Goods;
+import com.ll.weflea.boundedContext.goods.entity.Status;
+import com.ll.weflea.boundedContext.member.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class GoodsServiceTests {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @Test
+    void t001() throws Exception {
+        Member member = new Member();
+        GoodsController.CreateForm createForm = new GoodsController.CreateForm();
+
+        createForm.setTitle("테스트");
+        createForm.setArea("지역없음");
+        createForm.setStatus(Status.구매가능);
+        createForm.setSecurePayment(false);
+        createForm.setPrice(1000);
+        createForm.setDescription("상세설명");
+
+        for (int i = 1; i <= 120; i++) {
+            RsData<Goods> createRsData = goodsService.create(member, createForm);
+
+            if (createRsData.isFail()) {
+                throw new Exception("상품 등록에 실패하였습니다");
+            }
+        }
+    }
 }
